f5ab18281af14511ae19fd76b0dfab846f40770c
JohannH <johann.hertz2009@gmail.com>
2026-04-11 22:13:01 -0400

    finished the project with all its features

--- script.js
+++ script.js
@@ -7,33 +7,54 @@ const infoTitle = document.getElementById("infoTitle");
 const infoAuthor = document.getElementById("infoAuthor");
 const infoPages = document.getElementById("infoPages");
 const infoStatus = document.getElementById("infoStatus");
-const info = document.getElementById("info")
-
+const infoImg = document.getElementById("infoImg");
+const info = document.getElementById("info");
+const closeInfo = document.getElementById("closeInfo");
 let books = [];
 
+function createBook(title, author, pages, status, image, randId){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.randId = randId;
+    this.image = image;
+    this.info = function(){
+        console.log(`${this.title} written by ${this.author}, pages ${this.pages}, ${this.read? 'already read':'not read yet'}`)
+    }
+}
 
 addBook.addEventListener('click', function(){
     formContainer.style.visibility = "visible";
 });
 
-submit.addEventListener('click', function(e){
+form.addEventListener('submit', function(e){
     e.preventDefault();
 
     let customID = crypto.randomUUID();
     console.log(customID);
 
 
-    const bookButton = document.createElement("button");
+    
     const title = document.getElementById("name").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
     const status = document.getElementById("status").value === "true";
+    const image = document.getElementById("img").files[0];
+    const imageURL = URL.createObjectURL(image);
+
+    const bookButton = document.createElement("button");
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = title;
+    bookTitle.classList.add("bookInfo");
+    bookButton.classList.add("book");
     bookButton.id = customID;
 
+    bookButton.appendChild(bookTitle)
 
     console.log(bookButton.id);
 
-    const newBook = new createBook(title, author, pages, status, customID);
+    const newBook = new createBook(title, author, pages, status, image, customID);
 
     books.push(newBook);
 
@@ -44,9 +65,10 @@ submit.addEventListener('click', function(e){
 
                 info.style.visibility = "visible";
 
-                infoTitle.textContent = book.title;
-                infoAuthor.textContent = book.author;
-                infoPages.textContent = book.pages;
+                infoTitle.textContent = `Title: ${book.title}`;
+                infoAuthor.textContent = `by ${book.author}`;
+                infoPages.textContent = `Pages: ${book.pages}`;
+                infoImg.src = imageURL;
 
                 if(book.status){
                     infoStatus.textContent = "Already read.";
@@ -60,20 +82,14 @@ submit.addEventListener('click', function(e){
 
     library.appendChild(bookButton);
     formContainer.style.visibility = "hidden";
+    form.reset();
 
+});
 
+closeInfo.addEventListener('click', function(){
+    info.style.visibility = "hidden";
 });
 
 
 
-function createBook(title, author, pages, status, randId){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.randId = randId;
-    this.info = function(){
-        console.log(`${this.title} written by ${this.author}, pages ${this.pages}, ${this.read? 'already read':'not read yet'}`)
-    }
-}
 
